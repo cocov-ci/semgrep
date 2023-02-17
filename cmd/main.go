9dc72a2bb5e4d597f98acfcf5e3591d30459a90a
Felipe Mariotti <felipe.mtt95@gmail.com>
2023-02-17 18:11:26 -0300

    refactor: Implement application setup and entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import (
+	"github.com/cocov-ci/go-plugin-kit/cocov"
+	"github.com/cocov-ci/semgrep/plugin"
+)
 
-}
+func main() { cocov.Run(plugin.Run) }
--- plugin/run.go
+++ plugin/run.go
@@ -2,6 +2,9 @@ package plugin
 
 import (
 	"github.com/cocov-ci/go-plugin-kit/cocov"
+	"go.uber.org/zap"
+	"log"
+	"os"
 )
 
 const (
@@ -12,12 +15,39 @@ const (
 
 func Run(ctx cocov.Context) error {
 	s := newRunner(ccExec{})
-	res, err := s.run(ctx, nil)
+	l, err := setupLogger()
+	if err != nil {
+		log.Fatalf("error configuring logger %s", err.Error())
+	}
+
+	res, err := s.run(ctx, l)
 	if err != nil {
 		return err
 	}
 
-	_ = res
+	for _, r := range res {
+		err = ctx.EmitIssue(
+			r.kind, r.Path,
+			r.startLine(), r.startLine(),
+			r.message(), r.hashID(),
+		)
+
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+func setupLogger() (*zap.Logger, error) {
+	l, err := zap.NewProduction()
+	if os.Getenv("COCOV_ENV") == "development" {
+		opts := zap.Development()
+		l, err = zap.NewDevelopment(opts)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return l.With(zap.String("plugin", "semgrep")), nil
 }
